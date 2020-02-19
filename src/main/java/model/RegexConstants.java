a57bff6e2cc5c3fbe7fa1c1531ba8facad244b19
kelelas <kelelas2015@gmail.com>
2020-02-19 16:55:02 +0300

    branch2

--- src/main/java/model/RegexConstants.java
+++ src/main/java/model/RegexConstants.java
@@ -1,8 +1,19 @@
 package model;
 
 public class RegexConstants {
-    public static final String REGEX_FOR_SURNAME = "[A-Z]{1}[-a-z`]{0,19}[a-z]{1}";
-    public static final String REGEX_FOR_LOGIN = "[A-Z]{1}[a-z0-9]{7,20}";
-    //public static final String REGEX_TEST = "[A-Z]";
+    public static final String REGEX_FOR_SURNAME = "^[A-Z]{1}[ -a-z`]{0,19}[a-z]{1}$";
+    public static final String REGEX_FOR_LOGIN = "^[A-Za-z0-9]{7,20}$";
+    public static final String REGEX_FOR_NAME = "^[A-Z]{1}[ -a-z`]{0,19}[a-z]{1}$";
+    public static final String REGEX_FOR_SECOND_NAME = "^[A-Z]{1}[ -a-z`]{0,19}[a-z]{1}$";
+    public static final String REGEX_FOR_PHONE_NUMBER = "^([+])?\\(?([0-9]{3})\\)?([ .-]?)([0-9]{3})\\([0-9]{4})$";
+    public static final String REGEX_FOR_HOME_PHONE_NUMBER = "^([+])?\\(?([0-9]{3})\\)?([ .-]?)([0-9]{3})\\([0-9]{4})$";
+    public static final String REGEX_FOR_SECOND_PHONE_NUMBER = "^([+])?\\(?([0-9]{3})\\)?([ .-]?)([0-9]{3})\\([0-9]{4})$";
+    public static final String REGEX_FOR_COMMENT = ".*";
+    public static final String REGEX_FOR_GROUP = "[FRIENDS[FAMILY[OTHERS[WORK]]]]";
+    public static final String REGEX_FOR_EMAIL = "^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$";
+    public static final String REGEX_FOR_SKYPE = "^[ -`_@A-Za-z0-9]{7,20}$";
+    public static final String REGEX_FOR_ADDRESS_INDEX_OR_HOUSE_OR_FLAT ="[0-9]{5}";
+    public static final String REGEX_FOR_ADDRESS_TOWN_OR_STREET = "[A-Z]{1}[a-z]{25}";
+
 
 }
--- src/main/java/view/TextConstants.java
+++ src/main/java/view/TextConstants.java
@@ -1,7 +1,21 @@
 package view;
 
 public class TextConstants {
-    public static final String TEXT_FOR_SURNAME = "Please write your surname in format :";
-    public static final String TEXT_FOR_LOGIN = "Please write your login in format :";
-    public static final String INVALID_TEXT_FORMAT = "Invalid text format, please try one more time.";
+    public static final String TEXT_FOR_SURNAME = "input.string.textForSurname";
+    public static final String TEXT_FOR_NAME = "input.string.textForName";
+    public static final String TEXT_FOR_SECOND_NAME = "input.string.textForSecondName";
+    public static final String TEXT_FOR_EMAIL = "input.string.textForEmail";
+    public static final String TEXT_FOR_SKYPE = "input.string.textForSkype";
+    public static final String TEXT_FOR_INDEX = "input.string.textForIndex";
+    public static final String TEXT_FOR_CITY = "input.string.textForCity";
+    public static final String TEXT_FOR_STREET= "input.string.textForStreet";
+    public static final String TEXT_FOR_HOME_NUMBER = "input.string.textForHomeNumber";
+    public static final String TEXT_FOR_FLAT_NUMBER = "input.string.textForFlatNumber";
+    public static final String TEXT_FOR_PHONE_NUMBER = "input.string.textForPhoneNumber";
+    public static final String TEXT_FOR_HOME_PHONE_NUMBER = "input.string.textForHomePhoneNumber";
+    public static final String TEXT_FOR_SECOND_PHONE_NUMBER = "input.string.textForSecondPhoneNumber";
+    public static final String TEXT_FOR_LOGIN = "input.string.textForLogin";
+    public static final String TEXT_FOR_COMMENT = "input.string.textForComment";
+    public static final String TEXT_FOR_GROUP = "input.string.textForGroup";
+    public static final String INVALID_TEXT_FORMAT = "input.string.invalidTextFormat";
 }
